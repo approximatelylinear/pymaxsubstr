1768b9c318df8cb2bbce67e55ed5c0dea0ef833c
Matthew Berends <matthewberends@gmail.com>
2015-02-28 14:14:01 -0600

    Convert indentation to spaces.

--- pymaxsubstr/pymaxsubstr.py
+++ pymaxsubstr/pymaxsubstr.py
@@ -18,52 +18,52 @@ from .lib.pysais.pysais import get_maxsubst
 THIS_DIR = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
 
 def main():
-	"""
-	TBD
-	"""
+    """
+    TBD
+    """
 
 
 def substrs_to_trie(names):
-	name_str = u' '.join(names)
-	trie = marisa_trie.Trie(get_substrs(name_str))
-	return trie
+    name_str = u' '.join(names)
+    trie = marisa_trie.Trie(get_substrs(name_str))
+    return trie
 
 
 def load_trie(path):
-	return marisa_trie.Trie().load(path)
+    return marisa_trie.Trie().load(path)
 
 
 def get_substrs(s):
-	substrs = get_maxsubst(s)
-	return filter_substrs(substrs)
+    substrs = get_maxsubst(s)
+    return filter_substrs(substrs)
 
 
 def filter_substrs(substrs):
-	substrs = ( substr.strip() for substr in substrs )
-	substrs = ( substr for substr in substrs if len(substr) > 1 )
-	substrs = ( substr for substr in substrs if u' ' not in substr )
-	substrs = sorted(list(set(substrs)))
-	return substrs
+    substrs = ( substr.strip() for substr in substrs )
+    substrs = ( substr for substr in substrs if len(substr) > 1 )
+    substrs = ( substr for substr in substrs if u' ' not in substr )
+    substrs = sorted(list(set(substrs)))
+    return substrs
 
 
 def extract_features(st, trie):
-	#   ===============================================
-	def find_substrs(st, i, l):
-		for j in range(i, l):
-			prefix = st[i : j+1]
-			if not trie.has_keys_with_prefix(prefix):
-				break
-			if prefix in trie and len(prefix) > 1:
-				yield prefix
-	#   ===============================================
-	events_by_text = collections.Counter()
-	l = len(st)
-	for i in range(l):
-		substrs = find_substrs(st, i, l)
-		for p in substrs:
-			events_by_text[p] += 1
-	return events_by_text
+    #   ===============================================
+    def find_substrs(st, i, l):
+        for j in range(i, l):
+            prefix = st[i : j+1]
+            if not trie.has_keys_with_prefix(prefix):
+                break
+            if prefix in trie and len(prefix) > 1:
+                yield prefix
+    #   ===============================================
+    events_by_text = collections.Counter()
+    l = len(st)
+    for i in range(l):
+        substrs = find_substrs(st, i, l)
+        for p in substrs:
+            events_by_text[p] += 1
+    return events_by_text
 
 
 if __name__ == '__main__':
-		main()
+        main()
